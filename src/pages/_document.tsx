843275fc4cf6ba7453a15ce3c1e36635aee14ce0
Lucas.Liu <Lucas.Liu@skylinetw.com>
2023-02-15 16:40:14 +0800

    fix: 修正 styled-component ssr問題

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,21 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  static async getInitialProps(ctx: any) {
+    const originalRenderPage = ctx.renderPage;
+    const sheet = new ServerStyleSheet();
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App: any) => (props: any) => sheet.collectStyles(<App {...props} />),
+      });
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return {
+      ...initialProps,
+      styles: [sheet.getStyleElement()],
+    };
+  }
 }
+
+export default MyDocument;
