286a2a9ab22a3423fedeb9ba34312752e43b8d54
DESKTOP-1AKMAGM\Lucas <tdksr0505@gmail.com>
2023-02-19 22:06:07 +0800

    feat: proteced routes

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -1,11 +1,12 @@
 import React, { createContext, useState, useEffect, useRef } from 'react';
 import { TChildren } from '@/type';
-
+import { useCookies } from 'react-cookie';
 interface IUser {
   account: string;
   token: string;
 }
 const AUTH_KEY = 'auth';
+const LOGIN_KEY = 'isLogin';
 const contextDefaultValue = {
   user: { account: '', token: '' },
   login: (user: IUser) => {},
@@ -15,6 +16,7 @@ const contextDefaultValue = {
 
 export const AuthContext = createContext(contextDefaultValue);
 export const AuthProvider = ({ children }: TChildren) => {
+  const [cookies, setCookie] = useCookies();
   const [user, setUser] = useState<IUser>(contextDefaultValue.user);
   const [auth, setAuth] = useState<boolean>(false);
   const isMounted = useRef<boolean>(false);
@@ -41,12 +43,14 @@ export const AuthProvider = ({ children }: TChildren) => {
       account: data.account,
       token: data.token,
     });
+    setCookie(LOGIN_KEY, 1, { path: '/' });
   };
   const logout = () => {
     setUser({
       account: '',
       token: '',
     });
+    setCookie(LOGIN_KEY, 0, { path: '/' });
   };
   const providerValue = {
     user,
