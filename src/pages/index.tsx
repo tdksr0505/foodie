165e52dd2df8c78aa72610407f269dd9ae0764de
Lucas.Liu <Lucas.Liu@skylinetw.com>
2023-02-20 08:35:07 +0800

    fix: 暫時調整檔名

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import * as ListStyled from '../styles/styledListPage';
 import Filter from '../components/Filter';
-import List from '../components/List';
+import List from '../components/TmpList';
 import Button from '../components/Button';
 import useLoading from '../hooks/useLoading';
 import useAuth from '@/hooks/useAuth';
