00d2c61baaa947c7f9898e02e4ba7ada3ab68aac
DESKTOP-1AKMAGM\Lucas <tdksr0505@gmail.com>
2023-02-22 00:56:20 +0800

    feat: 調整背景呈現方式

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -7,8 +7,13 @@ const Container = styled.div`
   left: 0;
   width: 100vw;
   height: 100vh;
-  background: url(${bgImg.src}) top center no-repeat;
-  background-size: 1920px auto;
+`;
+
+const BgImg = styled.img`
+  position: absolute;
+  top: 0%;
+  left: 50%;
+  transform: translate3d(-50%, 0, 0);
 `;
 
 const Mask = styled.div`
@@ -23,6 +28,7 @@ const Mask = styled.div`
 const Background = () => {
   return (
     <Container>
+      <BgImg src={bgImg.src} />
       <Mask />
     </Container>
   );
